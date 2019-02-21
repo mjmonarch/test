b1129904983208b113fd3250b6691c2dcf362003
Sergii Kamenskyi <serg@apple.com>
2019-02-21 20:15:22 +0200

    ...

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -7,10 +7,10 @@ public class Main {
 
     public static void main(String[] args) {
         Set<Integer> set1 = new HashSet<>();
-        Set<Integer> set2 = new HashSet<>(set1);
         set1.add(1);
         set1.add(3);
         set1.add(5);
+        Set<Integer> set2 = new HashSet<>(set1);
         set2.add(6);
         set2.add(8);
         System.out.println("set1 = " + set1);
