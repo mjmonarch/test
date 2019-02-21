d5f5e5b70ec34ea2aa3ef88b48604a0cfaa9cc2b
Sergii Kamenskyi <serg@apple.com>
2019-02-21 20:25:14 +0200

    ...

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -10,11 +10,23 @@ public class Main {
         set1.add(1);
         set1.add(3);
         set1.add(5);
+
         Set<Integer> set2 = new HashSet<>(set1);
         set2.add(6);
         set2.add(8);
-        System.out.println("set1 = " + set1);
         System.out.println("set2 = " + set2);
+
+        Set<Integer> set3 = new HashSet<>();
+        set3.add(2);
+        set3.add(4);
+        set3.add(1);
+        set3.add(7);
+        System.out.println("set3 = " + set3);
+
+        //union
+        Set<Integer> union = new HashSet<>(set2);
+        union.addAll(set3);
+        System.out.println("union = " + union);
 
     }
 }
